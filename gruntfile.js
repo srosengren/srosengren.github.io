bf3f4c50a3ccd853871fea8461128227887a933b
Sebastian Rosengren <sebastian@rosengren.me>
2013-12-20 15:18:31 +0100

    gruntfile added

---
+++ gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          paths: ["./assets/styles/less"],
+          yuicompress: true,
+          compress: true
+        },
+        files: {
+          './assets/styles/rosengren.css': './assets/styles/rosengren.less'
+        }
+      }
+    },
+    copy: {
+      css : {
+        files: {
+          './_site/assets/styles/rosengren.css': './assets/styles/rosengren.css'
+        }
+      }
+    },
+    shell: {
+      jekyll: {
+        options: {
+          stdout: true
+        },
+        command: 'jekyll build'
+      }
+    },
+    watch: {
+      less: {
+        files: './assets/styles/*.less',
+        tasks: ['less:development', 'copy:css'],
+        options: {
+          livereload: true,
+          spawn: false
+        }
+      },
+      jekyll: {
+        files: [
+          '*.html', '*.yml', 'assets/js/**.js',
+          '_posts/**', '_includes/**'
+        ],
+        tasks: 'shell:jekyll',
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'watch');
+};
